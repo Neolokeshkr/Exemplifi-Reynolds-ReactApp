9361c99f875477582af800b695abbe8049639ed8
Lokesh Kumar <neolokeshkr@gmail.com>
2023-09-12 01:32:28 +0530

    Popper position fixes, Mobile Size Menu Alignment Fixes, clip-path removed

--- src/App.js
+++ src/App.js
@@ -10,46 +10,41 @@ import Tips from './assets/SVG/Tips.svg'
 
 
 import parchmentPaper from './assets/Images/12inParchmentRoll_packagingHero_1.16.19.png'
-import bakingCups from './assets/Images/500x335_RK_BkngCps_DS1_ew.png'
+import otherProducts from './assets/Images/500x335_RK_BkngCps_DS1_ew.png'
 import dessertBakeware from './assets/Images/500X335_RK_DssrtBkwr_DS1_ew.png'
 import standardWrap from './assets/Images/584541_RW_Std_500x335_card.png'
 import cutriteWaxPaper from './assets/Images/CutriteWaxPaper.png'
-import heroCarton from './assets/Images/RK SCL_Hero Carton 500x335_2.png'
+import cookingBag from './assets/Images/RK SCL_Hero Carton 500x335_2.png'
 
 import { usePopper } from 'react-popper';
 
-import {
-  StyledAppContainer,
-  LongEl,
-  Tooltip,
-  Button
-} from "./App.styled";
 import { useState } from "react";
-
-
 
 function App() {
   return (
     <>
-      <nav className="navbar navbar-expand-sm navbar-dark custom-blue">
-        <div className='container-fluid'>
+      <nav className="navbar navbar-expand-lg navbar-dark sticky-top custom-blue">
+        <div className='container'>
           <a className="navbar-brand" href="#">
             <img src={reynoldsLogo} alt="Company Logo" className="navbar-logo" />
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+            <span>
+              <i className='bi bi-list'></i>
+            </span>
           </button>
+
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav nav-fill w-100 text-white text-nowrap" >
-              <MenuItem svg={Products} menuItemName="PRODUCTS" className="nav-link active"/>
-              <VerticalLine />
+            <ul className="navbar-nav nav-fill text-white text-nowrap" >
+              <MenuItem svg={Products} menuItemName="PRODUCTS" className="nav-link active" />
+              {/* <VerticalLine /> */}
               <MenuItem svg={Recipes} menuItemName="RECIPES" />
-              <VerticalLine />
+              {/* <VerticalLine /> */}
               <MenuItem svg={Tips} menuItemName="TIPS & HOW-TOS" />
-              <VerticalLine />
+              {/* <VerticalLine /> */}
               <MenuItem svg={Holidays} menuItemName="HOLIDAYS & ENTERTAINING" />
-              <VerticalLine />
+              {/* <VerticalLine /> */}
               <MenuItem svg={Story} menuItemName="OUR STORY" />
             </ul>
             <SocialLinksAndSearch />
@@ -59,7 +54,7 @@ function App() {
     </>
   );
 }
- 
+
 
 const MenuItem = ({ svg, menuItemName }) => {
 
@@ -68,8 +63,9 @@ const MenuItem = ({ svg, menuItemName }) => {
   const [arrowElement, setArrowElement] = useState(null);
   const { styles, attributes } = usePopper(referenceElement, popperElement, {
     placement: "bottom",
+    // strategy: "absolute",
     modifiers: [
-      { name: "offset", options: { offset: [0, 15] } },
+      { name: "offset", options: { offset: [0, 30] } },
       { name: "arrow", options: { element: arrowElement } }
     ]
   })
@@ -86,12 +82,13 @@ const MenuItem = ({ svg, menuItemName }) => {
     <>
       <li
         ref={setReferenceElement}
-        className="nav-item ps-4 pe-4 "
+        className="nav-item ps-4 pe-4"
         onMouseEnter={showTooltip}
         onMouseLeave={hideTooltip}
       >
         <a className="nav-link" href="#" >
-          <img src={svg} style={{}} />
+          <img src={svg}
+          />
           <h4 className='text-wrap fw-bold'>{menuItemName}</h4>
         </a>
       </li>
@@ -103,6 +100,7 @@ const MenuItem = ({ svg, menuItemName }) => {
       >
         {
           menuItemName === 'PRODUCTS' && <PopoverProductMenu menuName={menuItemName} />
+          // "lokesh"
         }
         <span
           ref={setArrowElement}
@@ -118,7 +116,7 @@ const MenuItem = ({ svg, menuItemName }) => {
 const SocialLinksAndSearch = () => {
   return (
     <div className='text-white custom-serach-social-links'>
-      <div className='custom-social-links mb-2 bg-white'>
+      <div className='custom-social-links mb-2 '>
         <div><i className="bi bi-facebook ps-2 pb-3 custom-blue-icon  "></i></div>
         <div><i className="bi bi-twitter p-1 custom-blue-icon "></ i></div>
         <div><i className="bi bi-instagram p-1 custom-blue-icon  "></i></div>
@@ -126,7 +124,7 @@ const SocialLinksAndSearch = () => {
         <div><i className="bi bi-pinterest pe-3 custom-blue-icon  "></i></div>
       </div>
       <div className='custom-circle-search'>
-        <i className="bi bi-search"></i>
+        <i className="bi bi-search "></i>
       </div>
     </div>
   )
@@ -134,25 +132,24 @@ const SocialLinksAndSearch = () => {
 
 const PopoverProductMenu = ({ menuName }) => {
   return (
-    <div className="custom-product-item-grid">
-      {/* <div> */}
-      <img src={parchmentPaper} alt='Parchment Paper' />
-      {/* </div> */}
-      {/* <div> */}
-      <img src={bakingCups} alt='Parchment Paper' />
-      {/* </div> */}
-      {/* <div> */}
-      <img src={dessertBakeware} alt='Parchment Paper' />
-      {/* </div> */}
-      {/* <div> */}
-      <img src={standardWrap} alt='Parchment Paper' />
-      {/* </div> */}
-      {/* <div> */}
-      <img src={cutriteWaxPaper} alt='Parchment Paper' />
-      {/* </div> */}
-      {/* <div> */}
-      <img src={heroCarton} alt='Parchment Paper' />
-      {/* </div> */}
+    <div className="custom-grid">
+      <ProductItem src={standardWrap} productName="ALUMINIUM FOIL" productDescription="Perfect for grilling, baking and more." />
+      <ProductItem src={parchmentPaper} productName="PARCHMENT PAPER" productDescription="Oven-safe, non-stick performance for baking & roasting." />
+      <ProductItem src={dessertBakeware} productName="DISPOSEABLE BAKEWARE" productDescription="Great for any meal, with no cleanup." />
+      <ProductItem src={cookingBag} productName="COOKING BAGS & LINERS" productDescription="Fast & easy cleanup with no soaking or scrubbing" />
+      <ProductItem src={cutriteWaxPaper} productName="WAX & FREEZER PAPER" productDescription="Protects surfaces & keeps food fresh longer." />
+      <ProductItem src={otherProducts} productName="OTHER PRODUCTS" productDescription="Butcher Paper, Plastic Warp, and Baking Cups." />
+
+    </div>
+  )
+}
+
+const ProductItem = ({ src, productName, productDescription }) => {
+  return (
+    <div className='text-wrap text-center'>
+      <img src={src} alt='Parchment Paper' />
+      <h4 className='fw-bold' style={{ color: "#001489" }}>{productName}</h4>
+      <p>{productDescription}</p>
     </div>
   )
 }
@@ -164,65 +161,5 @@ const VerticalLine = () => {
     </div>
   )
 }
-
-const Example = () => {
-  const [referenceElement, setReferenceElement] = useState(null);
-  const [popperElement, setPopperElement] = useState(null);
-  const [arrowElement, setArrowElement] = useState(null);
-  const [placement, setPlacememt] = useState("bottom")
-  const { styles, attributes } = usePopper(referenceElement, popperElement, {
-    placement,
-    modifiers: [
-      { name: 'arrow', options: { element: arrowElement } },
-      { name: 'offset', options: { offset: [0, 10] } }
-    ],
-    // strategy: 'fixed'
-  });
-
-  const showTooltip = () => {
-    popperElement.setAttribute('data-show', true);
-  }
-
-  const hideTooltip = () => {
-    popperElement.removeAttribute('data-show')
-  }
-
-  return (
-    <StyledAppContainer>
-      <div>
-        {/* <LongEl /> */}
-        <Button
-          ref={setReferenceElement}
-          onMouseEnter={showTooltip}
-          onMouseLeave={hideTooltip}
-        >I'm a mystery</Button>
-        <Tooltip
-          ref={setPopperElement}
-          style={styles.popper}
-          {...attributes.popper}
-        >
-          <p>A tooltip with</p>
-          <span
-            ref={setArrowElement}
-            style={styles.arrrow}
-            {...attributes.arrow}
-            className="arrow"
-          >
-
-          </span>
-        </Tooltip>
-        {/* <LongEl /> */}
-      </div>
-    </StyledAppContainer>
-  );
-};
-
-
-// const Title = styled.h1`
-//   font-size: 1.5em;
-//   text-align: center;
-//   color: #BF4F74;
-// `;
-
 
 export default App;
